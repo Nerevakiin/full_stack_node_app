9c4be6ed9270b24f70be7dec5b118ab2789747fb
Nerevakiin <djmatisk@yahoo.gr>
2026-01-15 22:35:49 +0200

    slight corrections

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 import http from 'node:http'
-import path from 'node:path'
-import fs from 'node:fs/promises'
 import {serveStatic} from './utils/serveStatic.js'
 
 const PORT = 8000
@@ -9,18 +7,10 @@ const PORT = 8000
 const __dirname = import.meta.dirname
 
 const server = http.createServer(async (req, res) => {
-
-    const pathToResource = path.join(__dirname, 'public', 'index.html') // The absolute path +  relative path (the folder + the file we want)
-
-
-    const content = await fs.readFile(pathToResource, 'utf-8')
     
-    serveStatic(__dirname)
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end(content)
-
-
+    // const pathToResource = path.join(__dirname, 'public', 'index.html') // The absolute path +  relative path (the folder + the file we want)
+    
+    await serveStatic(req, res, __dirname)
 
 })
 
--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,7 +1,36 @@
 import path from 'node:path'
+import fs from 'node:fs'
+import { sendResponse } from './sendResponse.js'
+import { getContentType } from './getContentType.js'
 
 
-export function serveStatic(baseDir){
-    const filePath = path.join(baseDir, 'public', 'index.html')
-    console.log(filePath)
+export async function serveStatic(req, res, baseDir){
+    
+    const publicDir = path.join(baseDir, 'public')
+    const filePath = path.join(
+        publicDir,
+        req.url === '/' ? 'index.html' : req.url
+    )
+
+    const ext = path.extname(filePath)
+
+    const contentType = getContentType(ext)
+    
+
+    try {
+        
+        const content = await fs.readFile(filePath) //getting the file we want and 'serving' it
+        sendResponse(res, 200, contentType, content)
+
+    } catch (err){
+        
+        if (err.code === 'ENOENT') {
+            
+            const content = await fs.readFile(path.join(publicDir, '404.html'))
+            sendResponse(res, 404, 'text/html', content)
+
+        } else {
+            sendResponse(res, 500, 'text/html', `<html><h1>Server Error: ${err.code}</h1></html>`)
+        }
+    }
 }
