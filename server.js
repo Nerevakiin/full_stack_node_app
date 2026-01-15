0ffc1ec872e52a0005d324d5827526508c23ea18
Nerevakiin <djmatisk@yahoo.gr>
2026-01-15 16:33:42 +0200

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+import http from 'node:http'
+import path from 'node:path'
+
+const PORT = 8000
+
+
+const __dirname = import.meta.dirname
+
+const server = http.createServer(async (req, res) => {
+
+    const absPathToResource = path.join(__dirname, 'public', 'index.html') // The absolute path +  relative path (the folder + the file we want)
+    const relPathToResource = path.join('public', 'index.html')
+    console.log('absolute: ', absPathToResource)
+    console.log('relative: ', relPathToResource)
+
+
+
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><h1>The server is working</h1></html>')
+})
+
+
+server.listen(PORT, () => console.log(`server is listening on port: ${PORT}`))
+
---
+++ utils/serveStatic.js
@@ -0,0 +1,8 @@
+import path from 'node:path'
+
+export function testPath() {
+
+  const relPathToResource = path.join('public', 'index.html')
+  console.log(testPath)
+
+}
